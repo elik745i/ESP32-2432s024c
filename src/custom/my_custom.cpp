bfe6bd4b572bfe272d73c2ec60eccabeb1fbca61
elik <elik745i@gmail.com>
2023-11-08 14:51:30 +0300

    added smooth bg color transition for bar element from red to green

--- src/custom/my_custom.cpp
+++ src/custom/my_custom.cpp
@@ -77,6 +77,7 @@ void custom_loop()
         batteryFraction = map(constrain(currentVoltage, minVoltage, maxVoltage) * 1000, minVoltage * 1000, maxVoltage * 1000, 0, 100);
         illum = analogRead(illum_read);
         last_blink = millis();
+        
         updateBatteryDisplay(11, 2, batteryFraction);
         updateBatteryDisplay(9, 9, batteryFraction);
         updateBatteryDisplay(0, 7, batteryFraction);
@@ -108,12 +109,25 @@ void custom_loop()
 
 }
 
+//search bar element and apply adata float value to it
 void updateBatteryDisplay(uint8_t page, uint8_t id, float adata) {
     lv_obj_t* widget = hasp_find_obj_from_page_id(page, id);
     if (!widget) return; // object doesn't exist
+
+    // Calculate the color based on battery percentage
+    uint8_t red = constrain(map(adata, 0, 100, 255, 0), 0, 255);
+    uint8_t green = constrain(map(adata, 0, 100, 0, 255), 0, 255);
+    uint8_t blue = 0; // Assuming no blue component
+
+    // Set the calculated color
+    lv_color_t color = LV_COLOR_MAKE(red, green, blue);
+    lv_obj_set_style_local_bg_color(widget, LV_OBJ_PART_MAIN, LV_STATE_DEFAULT, color);
+
+
     lv_bar_set_value(widget, adata, LV_ANIM_OFF);
 }
 
+//search label element and apply adata text value to it
 void updateTextDisplay(uint8_t page, uint8_t id, const char* text) {
     lv_obj_t* widget = hasp_find_obj_from_page_id(page, id);
     if (!widget) return; // object doesn't exist
